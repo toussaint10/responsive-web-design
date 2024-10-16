4cce18f0b873fa884b67b9554bca4acda13a4eb6
toussaint10 <l.tona@alustudent.com>
2024-10-16 15:15:14 +0200

    Responsive web design Resume

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // Form Validation Example
-document.querySelector('form').addEventListener('submit', function(e) {
+document.querySelector('form').addEventListener('submit', function (e) {
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const message = document.getElementById('message').value.trim();
+// Collect values from input labels
 
     if (name === '' || email === '' || message === '') {
         alert('Please fill in all fields');
         e.preventDefault();
     }
-
-    alert(`${name} ${email} ${message}`)
+// Confirm all boxes are filled
+    alert("Thank you your information of: " + name + " " + email + " " + message + " has been saved")
 });
+
+document.getElementById('name').addEventListener('keydown', function (e) {
+    if (!e.key.match(/[a-zA-Z]|\s/)) {
+        e.preventDefault()
+    }
+})
+// Namebox character input confirmation
+
+function togglenav() {
+    let button = document.getElementById('menunav')
+    button.classList.toggle('hiddennav')
+}
+// Toggle Click setup
